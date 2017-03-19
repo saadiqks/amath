c12b1ae4fd0aae9fe795b734a5ce2e8be5b02c21
Saadiq Shaik <saadiqks@gmail.com>
2017-03-18 23:31:05 -0400

    Made a few formatting changes

--- src/amath.c
+++ src/amath.c
@@ -1,38 +1,39 @@
 #include <stdio.h>
 #include "util.h"
 #include "amath.h"
-
 #include "amath.leg.c"
 
-struct greg_data *mk_greg_data(const char *input)
-{
+struct greg_data *mk_greg_data(const char *input) {
 	struct greg_data *D = malloc(sizeof(struct greg_data));
 
-	if (D == NULL)
-		return NULL;
-
+	if (D == NULL) {
+	   return NULL;
+	}
+		
 	D->input = input;
 	D->result = mk_empty(NODE_RAW);
 
 	return D;
 }
 
-char *amath_to_mathml(const char *text)
-{
+char *amath_to_mathml(const char *text) {
 	GREG g;
 	char *result;
 	struct greg_data *data;
 
 	data = mk_greg_data(text);
 
-	if (strlen(text) <= 0 || data == NULL)
-		return NULL;
+	if (strlen(text) <= 0 || data == NULL) {
+           return NULL;
+	}
 
 	yyinit(&g);
 	g.data = data;
+	
 	while (yyparse(&g));
 	result = strdup(data->result->text);
 	yydeinit(&g);
+	
 	free_node(data->result);
 	free(data);
 
